315e8e8ee2bad24569d63d66ef2c7b4ee6a873ea
6410685074 <88650235+6410685074@users.noreply.github.com>
2023-10-13 12:57:51 +0700

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x(qrfh%lme$kqpln!5+uy*94tme$*xf!al1pw2(0w-k0dloqt3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kunkerdthaisong.pythonanywhere.com']
 
 
 # Application definition
